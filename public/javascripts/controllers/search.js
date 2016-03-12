663f8308076a7ff6b66af8df3e388ab4213a68e5
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-12 14:15:52 -0600

    added main page styles

--- public/javascripts/controllers/search.js
+++ public/javascripts/controllers/search.js
@@ -7,17 +7,25 @@ app.controller('searchCtrl', ["$http", function($http){
 
   //initialize user-searched tag
   self.searchTag = "";
+  // searched by default
+  self.userSearched = false;
+
+  self.queryApi = () => {
+    self.userSearched = true;
+  }
 
   // get search data from api
   self.outputTags = (searchInput) => {
     console.log("inside click function");
     console.log(searchInput);
+    self.queryApi();
   };
 
   self.outputTagsKeypress = (event, searchInput) => {
     // check if enter key was pressed
     if(event.keyCode === 13){
       console.log(searchInput);
+      self.queryApi();
     }
   };
 
